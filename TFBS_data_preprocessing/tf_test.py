8f220e4d557a09148fd29b7f201711ffe3a264e3
AzureAD\Curigin <ahnty0122@naver.com>
2021-03-09 11:23:44 +0900

    return 여러개

--- TFBS_data_preprocessing/tf_test.py
+++ TFBS_data_preprocessing/tf_test.py
@@ -10,7 +10,7 @@ def poolcontext(*args, **kwargs):
     yield pool
     pool.terminate()
 
-# SW 결과 aligned query sequence 반환해주는 함수
+'''# SW 결과 aligned query sequence 반환해주는 함수
 def aligned_query_sequence(symbolA, symbolB):
 	# striped SW symbol 생성 및 SW score 계산
 	symbolA = StripedSmithWaterman(symbolA)
@@ -36,7 +36,7 @@ def sw_score(symbolA, symbolB):
 	# striped SW symbol 생성 및 SW score 계산
 	symbolA = StripedSmithWaterman(symbolA)
 	result = symbolA(str(symbolB))
-	return result['optimal_alignment_score']
+	return result['optimal_alignment_score']'''
 
 # mismatch 계산함수
 def mismatch(p,q):
@@ -48,6 +48,11 @@ def mismatch(p,q):
 			dist += 1
 	return dist
 
+
+def all_SW(symbolA, symbolB):
+	symbolA = StripedSmithWaterman(symbolA)
+	result = symbolA(str(symbolB))
+	return pd.Series([result['aligned_query_sequence'],len(result['aligned_query_sequence']), result['aligned_target_sequence'],result['optimal_alignment_score']])
 
 tsg = pd.read_csv('Tumor_suppressor_gene_list.csv')
 ocg = pd.read_csv('oncogene_list.csv')
@@ -71,12 +76,13 @@ def check_tsg(TF):
         return True
 
 if __name__ == '__main__':
-	
+	# 경고 제거
+	pd.set_option('mode.chained_assignment',  None)
 	# artificial promoter sequence
 	a_p = 'TGCCGCTAGAGGACGCCGTGGCCGCGCTCAGCGGTGCCGCTAGAGGCGCGCGGCCGCGGTCACCCTACCTTCCTCCGGGCCTCTCAGAGGAAATTAAGCGCTGCTGTCCACTGGCCCCACGGCACCGGGACACTGGGTGGATGAGTCCAGTCCCGCGGCCTTTCTCTTCACCGGAGACACCGATTAGATTTCAAGCCCATTTTATGACCCTCCGGACTTCCGCAGCGCTTATTTCCCTCCTGAATGCGGAGCAAGATGACTCCCACCCACCCCCGGGGCTGGGGGCGAGGCAGAACTGCGA'
 
 	# tfbs, tumor supressor gene, oncogene 파일 불러오기
-	tfbs = pd.read_csv('tfbs_test.txt', delimiter = '\t')
+	tfbs = pd.read_csv('tfbs_test.txt', delimiter = '\t', low_memory=False)
 	# tsg = pd.read_csv('Tumor_suppressor_gene_list.csv')
 	# ocg = pd.read_csv('oncogene_list.csv')
 
@@ -97,20 +103,16 @@ if __name__ == '__main__':
 		symbolB = result['Artificial_promoter_sequence']
 		
 		# SymbolA, symbolB zip으로 묶어서 starmap으로 인자 전달 및 함수 실행
-		result['TF_sequence(target)'] = pool.starmap(aligned_target_sequence, zip(symbolA, symbolB))
-		result['Artificial promoter sequence(query)'] = pool.starmap(aligned_query_sequence, zip(symbolA, symbolB))
-		result['length(TF)'] = pool.starmap(length_TF, zip(symbolA, symbolB))
-		result['SW_score'] = pool.starmap(sw_score, zip(symbolA, symbolB))
-
+		result[['Artificial promoter sequence(query)','length(TF)','TF_sequence(target)','SW_score']] = pool.starmap(all_SW, zip(symbolA, symbolB))
+		print(result)
 		# mismatch 함수 실행
 		p = result['TF_sequence(target)']
 		q = result['Artificial promoter sequence(query)']
 		result['mismatch'] = pool.starmap(mismatch, zip(p, q))
+		print(result)
 
 		# Oncogene 열 생성
 		TF = result['TF']
-		print(type(result['TF']))
-		print(type(result['TF'][0]))
 		result['OCG'] = pool.map(check_ocg, TF)
 
 		# Tumor suppressor gene 열 생성
@@ -119,4 +121,4 @@ if __name__ == '__main__':
 	# 컬럼병 변경
 	result = result[['Artificial_promoter_sequence', 'TF', 'TF_sequence(target)', 'Artificial promoter sequence(query)', 'mismatch', 'length(TF)', 'chromosome_number', 'OCG', 'TSG']]
 	print(result)
-	result.to_csv('tfbs_result_test.txt', sep = '\t', index = False)
+	result.to_csv('testresult.txt', sep = '\t', index = False)
